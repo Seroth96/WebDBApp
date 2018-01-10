937aa20cfc92de187c7c89008a67e757a6a39907
Paweł Bokotko <pbokotko@gmail.com>
2018-01-10 21:13:50 +0100

    Adding angular calendar view and some css

--- Login/Controllers/CalendarController.cs
+++ Login/Controllers/CalendarController.cs
@@ -15,47 +15,39 @@ namespace Login.Controllers
         }
 
         public JsonResult GetEvents()
-        {
-            //Here MyDatabaseEntities is our entity datacontext (see Step 4)
-            //using (MyDatabaseEntities dc = new MyDatabaseEntities())
-            //{
-            //    var v = dc.Events.OrderBy(a => a.StartAt).ToList();
-            //    
-            //}
-
-            
+        {   
             var events = new List<CalendarEvent>();
 
             events.Add(new CalendarEvent
             {
                 Title = "test2",
                 Description = "wadwadwd bla bla",
-                StartAt = DateTime.Now.AddDays(5).ToString(@"dd\/MM\/yyyy HH:mm:ss"),
-                EndAt = DateTime.Now.AddDays(5).AddHours(2).ToString(@"dd\/MM\/yyyy HH:mm:ss"),
+                StartAt = DateTime.Now.AddDays(5).ToString(@"MM\/dd\/yyyy HH:mm:ss"),
+                EndAt = DateTime.Now.AddDays(5).AddHours(2).ToString(@"MM\/dd\/yyyy HH:mm:ss"),
                 IsFullDay = false
             });
             events.Add(new CalendarEvent
             {
                 Title = "test5",
                 Description = "opis bla bla",
-                StartAt = DateTime.Now.AddDays(10).ToString(@"dd\/MM\/yyyy HH:mm:ss"),
-                EndAt = DateTime.Now.AddDays(10).AddHours(7).ToString(@"dd\/MM\/yyyy HH:mm:ss"),
+                StartAt = DateTime.Now.AddDays(10).ToString(@"MM\/dd\/yyyy HH:mm:ss"),
+                EndAt = DateTime.Now.AddDays(10).AddHours(7).ToString(@"MM\/dd\/yyyy HH:mm:ss"),
                 IsFullDay = false
             });
             events.Add(new CalendarEvent
             {
                 Title = "test8",
                 Description = "opiskupa dupa",
-                StartAt = DateTime.Now.AddDays(3).ToString(@"dd\/MM\/yyyy HH:mm:ss"),
-                EndAt = DateTime.Now.AddDays(3).AddHours(2).ToString(@"dd\/MM\/yyyy HH:mm:ss"),
+                StartAt = DateTime.Now.AddDays(3).ToString(@"MM\/dd\/yyyy HH:mm:ss"),
+                EndAt = DateTime.Now.AddDays(3).ToString(@"MM\/dd\/yyyy HH:mm:ss"),
                 IsFullDay = true
             });
             events.Add(new CalendarEvent
             {
                 Title = "test234",
                 Description = "opis bwadwad",
-                StartAt = DateTime.Now.AddDays(-4).ToString(@"dd\/MM\/yyyy HH:mm:ss"),
-                EndAt = DateTime.Now.AddDays(-4).AddHours(2).ToString(@"dd\/MM\/yyyy HH:mm:ss"),
+                StartAt = DateTime.Now.AddDays(-4).ToString(@"MM\/dd\/yyyy HH:mm:ss"),
+                EndAt = DateTime.Now.AddDays(-4).AddHours(2).ToString(@"MM\/dd\/yyyy HH:mm:ss"),
                 IsFullDay = false
             });
 
--- Login/Scripts/calendar/MyApp.js
+++ Login/Scripts/calendar/MyApp.js
@@ -1,13 +1,12 @@
-﻿var app = angular.module('myApp', ['ui.calendar']);
+﻿var app = angular.module('myApp', ['ui.calendar', 'ui.bootstrap']);
 app.controller('myNgController', ['$scope', '$http', 'uiCalendarConfig', function ($scope, $http, uiCalendarConfig) {
-    
-    $scope.SelectedEvent = null;
+
+    $scope.SelectedEvent =null;
     var isFirstTime = true;
- 
+
     $scope.events = [];
-    $scope.eventSources = [$scope.events];
- 
- 
+    $scope.eventSources = [$scope.events];   
+
     //Load events from server
     $http.get('/Calendar/GetEvents', {
         cache: true,
@@ -15,18 +14,18 @@ app.controller('myNgController', ['$scope', '$http', 'uiCalendarConfig', functio
     }).then(function (data) {
         $scope.events.slice(0, $scope.events.length);
         angular.forEach(data.data, function (value) {
-            alert(parseInt(value.StartAt.substr(6)));
+           // alert(new Date(value.StartAt) + "\n" + new Date(value.EndAt));
             $scope.events.push({
                 title: value.Title,
                 description: value.Description,
-                start: new Date(parseInt(value.StartAt.substr(6))),
-                end: new Date(parseInt(value.EndAt.substr(6))),
-                allDay : value.IsFullDay,
+                start: new Date(value.StartAt),
+                end: new Date(value.EndAt),
+                allDay: value.IsFullDay,
                 stick: true
             });
         });
     });
- 
+
     //configure calendar
     $scope.uiConfig = {
         calendar: {
@@ -34,9 +33,9 @@ app.controller('myNgController', ['$scope', '$http', 'uiCalendarConfig', functio
             editable: true,
             displayEventTime: false,
             header: {
-                left: 'month basicWeek basicDay agendaWeek agendaDay',
+                left: 'month basicWeek basicDay agendaWeek agendaDay',                
                 center: 'title',
-                right:'today prev,next'
+                right: 'today prev,next'
             },
             eventClick: function (event) {
                 $scope.SelectedEvent = event;
@@ -50,5 +49,5 @@ app.controller('myNgController', ['$scope', '$http', 'uiCalendarConfig', functio
             }
         }
     };
- 
-}])
+
+}]);
